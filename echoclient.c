e0aee133a6d02e516713a636e5c032c82a21cc8e
Defdava <90128374+Defdava@users.noreply.github.com>
2025-09-09 15:00:19 +0700

    Update echoclient.c

--- echoclient.c
+++ echoclient.c
@@ -24,7 +24,7 @@ int main() {
     servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // localhost
 
-    // konek ke server
+    // connect ke server
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("connect error");
         exit(1);
